5be1ca517bdf98fa8d30b85ca3bfe27c69b432b7
Erik Silva Maia <erikm384@gmail.com>
2025-02-12 23:16:57 -0300

    feet: LL(1)

--- src/cc/ParseException.java
+++ src/cc/ParseException.java
@@ -108,7 +108,7 @@ public class ParseException extends Exception {
       }
       expected.append(EOL).append("    ");
     }
-    String retval = "Encontrado \"";
+    String retval = "Encountered \"";
     Token tok = currentToken.next;
     for (int i = 0; i < maxSize; i++) {
       if (i != 0) retval += " ";
@@ -123,7 +123,7 @@ public class ParseException extends Exception {
       tok = tok.next;
     }
     if (currentToken.next != null) {
-      retval += "\" na linha " + currentToken.next.beginLine + ", coluna " + currentToken.next.beginColumn;
+      retval += "\" at line " + currentToken.next.beginLine + ", column " + currentToken.next.beginColumn;
     }
     retval += "." + EOL;
     
@@ -132,9 +132,9 @@ public class ParseException extends Exception {
         // Nothing to add here
     } else {
 	    if (expectedTokenSequences.length == 1) {
-	      retval += "esperando:" + EOL + "    ";
+	      retval += "Was expecting:" + EOL + "    ";
 	    } else {
-	      retval += "Foi identificado:" + EOL + "    ";
+	      retval += "Was expecting one of:" + EOL + "    ";
 	    }
 	    retval += expected.toString();
     }
